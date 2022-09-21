47df636155ee8a8f5a13334c9d003d1e0378f492
Nnamani John <nnamanijohn123@gmail.com>
2022-09-21 13:23:16 -0700

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,16 +13,7 @@ int _strcmp(char *s1, char *s2)
 
 	while ((s1[i] != '\0') && (s2[i] != '\0'))
 	{
-		if (s1[i] < s2[i])
-			return (-15);
-		if (s1[i] > s2[i])
-			return (15);
 		i++;
 	}
-	if (s1[i + 1] == s2[i + 1])
-		return (0);
-	else if (s1[i + 1] < s2[i + 1])
-		return (-15);
-	else
-		return (15);
+	return (s1[i + 1] - s2[i + 1]);
 }
