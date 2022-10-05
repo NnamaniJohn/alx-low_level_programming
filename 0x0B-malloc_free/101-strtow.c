0cb039a7c8909779017f1a31419ddf8c30e96fd1
Nnamani John <nnamanijohn123@gmail.com>
2022-10-04 20:03:49 -0700

    string to words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,32 +11,28 @@ char **strtow(char *str)
 {
 	char *w;
 	char **s;
-	int i = 0, j = 0, k, words = 1;
+	int i = 0, j = 0, k, words = 1, len = 0;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
 	while (str[i])
 	{
-		if (str[i] == ' ' && str[i - 1] && str[i - 1] != ' ')
+		if (str[i] == ' ' && str[i - 1] != ' ')
 			words++;
 		i++;
 	}
-	s = (char **) malloc(sizeof(char *) *  words);
+	s = (char **) malloc(sizeof(char *) *  (words + 1));
 	if (s == NULL)
 		return (NULL);
 	words = 0;
-	for (i = 0; str[i]; i++)
+	len = i;
+	for (i = 0; i <= len; i++)
 	{
-		if (str[i] == ' ' && str[i - 1] && str[i - 1] != ' ')
+		if ((str[i] == ' ' && str[i - 1] != ' ') || str[i] == '\0')
 		{
 			w = (char *) malloc(sizeof(char) * (j + 1));
 			if (w == NULL)
-			{
-				for (; words >= 0; words--)
-					free(s[words]);
-				free(s);
 				return (NULL);
-			}
 			s[words] = w;
 			words++;
 			for (k = 0; k < j; k++)
@@ -47,5 +43,6 @@ char **strtow(char *str)
 		else if (str[i] != ' ')
 			j++;
 	}
+	s[words] = NULL;
 	return (s);
 }
