fb4b47e18d9d6255e2077febb7f3a4fae9e02e2f
Nnamani John <nnamanijohn123@gmail.com>
2022-10-06 16:50:55 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,13 +17,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
+	{
 		p = malloc(new_size);
+		return (p);
+	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
 	memcpy(p, ptr, old_size);
 	free(ptr);
 	return (p);
