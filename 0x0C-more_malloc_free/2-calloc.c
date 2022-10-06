c85a5128550f1bb16bed313448732678eafaf8e0
Nnamani John <nnamanijohn123@gmail.com>
2022-10-06 15:18:14 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,6 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
 	void *p;
 
 	if (nmemb == 0 || size == 0)
@@ -18,9 +17,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-	{
-		p[i] = 0;
-	}
 	return (p);
 }
