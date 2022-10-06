07eb53537cf3882f224ee3c444a68b09036deadb
Nnamani John <nnamanijohn123@gmail.com>
2022-10-06 05:28:23 -0700

    malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,11 +4,13 @@
 /**
  * malloc_checked -  function that allocates memory using malloc
  * @b: int
+ * Return: void pionter
  */
 
 void *malloc_checked(unsigned int b)
 {
 	void *p;
+
 	p = malloc(b);
 	if (p == NULL)
 		exit(98);
