5de8a7503893b215b79945bace8adfc7071e79bd
Nnamani John <nnamanijohn123@gmail.com>
2022-10-10 06:18:20 -0700

    structures and typedef

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * free_dog - free memory
@@ -8,7 +9,7 @@
 
 void free_dog(dog_t *d)
 {
-	free(d.name);
-	free(d.owner);
+	free(d->name);
+	free(d->owner);
 	free(d);
 }
