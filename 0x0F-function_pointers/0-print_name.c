5cdfd568af953415da3948395af3c4ffcbc4d67b
Nnamani John <nnamanijohn123@gmail.com>
2022-10-12 13:22:56 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: string to print
+ * @f: funtion use in printing
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+    f(name);
+}
