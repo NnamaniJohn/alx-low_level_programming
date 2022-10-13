5051577f3d6ee9d3b58453643b5887f28381615f
Nnamani John <nnamanijohn123@gmail.com>
2022-10-12 17:14:19 -0700

    opcode

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** main - program that prints the opcodes 
+ * @argc: args count
+ * @argv: args value
+ * Return: int
+ */
+
+int main(int argc, **argv)
+{
+	int num;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	num = atoi(argv[1]);
+	if (num < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < num; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
