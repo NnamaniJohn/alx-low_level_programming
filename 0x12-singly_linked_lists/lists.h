8ba44e500b6418f4004793b7e7b39cd58c396965
Nnamani John <nnamanijoh123@gmail.com>
2022-10-20 11:02:11 +0000

    print all elements of a list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,7 +6,15 @@
 
 typedef struct list list_t;
 
-struct list {
+/**
+ * struct list - list
+ * @str: string
+ * @len: len of string
+ * @next: pointer to next element of list
+ */
+
+struct list
+{
 	char *str;
 	int len;
 	list_t *next;
