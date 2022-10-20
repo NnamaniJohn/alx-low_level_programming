51260e262c376b42b07cf5879226d8e1a7e8602f
Nnamani John <nnamanijoh123@gmail.com>
2022-10-20 11:22:12 +0000

    add node

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct list list_t;
 
@@ -23,5 +24,6 @@ struct list
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /* MAIN_H */
