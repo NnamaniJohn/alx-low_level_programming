ae1cf4e31027c171efa58a8bdffb1874187e2211
Nnamani John <nnamanijoh123@gmail.com>
2022-10-20 11:36:47 +0000

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,12 +11,12 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
+	if (head == NULL || str == NULL)
+		return (NULL);
 	new = malloc(sizeof(list_t));
-	if (new == NULL || str == NULL)
+	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
-	if (new->str == NULL)
-		return (NULL);
 	new->len = 0;
 	while (new->str[new->len])
 		new->len++;
