aca750db71617d1e0b687a8983fae8110947b19c
Nnamani John <nnamanijoh123@gmail.com>
2022-10-25 15:45:51 +0000

    free list safe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,20 +3,31 @@
 
 size_t free_listint_safe(listint_t **h)
 {
-	size_t size = 0;
-	listint_t *temp;
+	listint_t *current, *runner, *head;
+	size_t h_i, r_i;
 
-	if (h != NULL)
+	if (h == NULL || *h == NULL)
+		return (0);
+	current = *h;
+	head = *h;
+	h_i = 0;
+	while (head != NULL)
 	{
-		while (*h != NULL)
+		runner = *h;
+		for (r_i = 0; r_i < h_i; r_i++)
 		{
-			temp = (*h)->next;
-			(*h)->next = NULL;
-			free(*h);
-			*h = temp;
-			size++;
+			if (runner == current)
+			{
+				*h = NULL;
+				return (h_i);
+			}
+			runner = runner->next;
 		}
-		*h = NULL;
+		current = head->next;
+		free(head);
+		head = current;
+		h_i++;
 	}
-	return (size);
+	*h = NULL;
+	return (h_i);
 }
