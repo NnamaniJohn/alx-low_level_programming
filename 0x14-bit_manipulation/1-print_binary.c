c5ea66c72fe0af36f7d9535e099f73412d0c8889
Nnamani John <nnamanijoh123@gmail.com>
2022-10-28 01:42:31 +0000

    print bin

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
- * power - power function
- * @base: base
- * @exp: exp
- * Return: int
- */
-
-unsigned long int power(unsigned long int base, unsigned long int exp)
-{
-	unsigned long int result = 1;
-
-	while (exp != 0)
-	{
-		result *= base;
-		--exp;
-	}
-	return (result);
-}
-
-/**
  * prt_bin - print binary
  * @pow: power
  * @n: number
@@ -28,16 +9,15 @@ unsigned long int power(unsigned long int base, unsigned long int exp)
 
 void prt_bin(unsigned long int pow, unsigned long int n)
 {
-	unsigned long int b;
+	if (n < 1)
+		return;
 
-	if (n >= power(2, pow - 1))
-		b = 1;
+	_divide(n >> 1);
+
+	if (n & 1)
+		_putchar('1');
 	else
-		b = 0;
-	_putchar(b + '0');
-	n = n - (b * power(2, pow - 1));
-	if (pow - 1 > 0)
-		prt_bin(pow - 1, n);
+		_putchar('0');
 }
 
 /**
@@ -48,19 +28,11 @@ void prt_bin(unsigned long int pow, unsigned long int n)
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int base = 2;
-	unsigned long int pow = 1;
-
-	if (n < 2)
-		_putchar(n + '0');
-	else
+	if (n == 0)
 	{
-		while (base <= n)
-		{
-			pow++;
-			base *= 2;
-		}
-		prt_bin(pow, n);
+		_putchar('0');
+		return;
 	}
 
+	_divide(n);
 }
