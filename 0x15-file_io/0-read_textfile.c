509d1665fd0eadc68058f739b92018a785335758
Nnamani John <nnamanijoh123@gmail.com>
2022-10-31 22:13:10 +0000

    read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	read(fd, buf, letters)
+	read(fd, buf, letters);
 	close(fd);
 	let = write(STDOUT_FILENO, buf, letters);
 	if (let == -1 || let < (ssize_t) letters)
