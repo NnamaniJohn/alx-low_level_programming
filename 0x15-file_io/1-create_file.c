edc51c0b8e603438de5e876b2347d96923489284
Nnamani John <nnamanijoh123@gmail.com>
2022-10-31 22:46:12 +0000

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of file
+ * @text_context: context of file
+ * Return: int
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t len = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_APPEND | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	while (text_content[len])
+		len++;
+	write(fd, text_content, len);
+	close(fd);
+
+	return (1);
+}
