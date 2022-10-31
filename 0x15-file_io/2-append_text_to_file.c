5c86182940472c4c02a727c37f9e0a9c116aeff7
Nnamani John <nnamanijoh123@gmail.com>
2022-10-31 23:09:19 +0000

    append to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - function that creates a file
+ * @filename: name of file
+ * @text_content: context of file
+ * Return: int
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t len = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd == -1)
+		return (-1);
+	while (text_content[len])
+		len++;
+	write(fd, text_content, len);
+	close(fd);
+
+	return (1);
+}
