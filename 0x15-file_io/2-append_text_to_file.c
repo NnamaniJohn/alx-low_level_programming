d8ad14adc517c0e5b8b66a7fdbe4e1ffbb49d8f5
Nnamani John <nnamanijoh123@gmail.com>
2022-10-31 23:10:57 +0000

    append to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	while (text_content[len])
 		len++;
-	write(fd, text_content, len + 1);
+	write(fd, text_content, len);
 	close(fd);
 
 	return (1);
