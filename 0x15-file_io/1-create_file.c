4710f9112aa19313c74e1e95c0d880b51bffb59f
Nnamani John <nnamanijoh123@gmail.com>
2022-11-01 10:02:06 +0000

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,19 +14,19 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
 	while (text_content[len])
 		len++;
-	if (text_content)
-	{
-		write(fd, text_content, len);
-	}
-	else
+	if (text_content == NULL)
 	{
 		close(fd);
 		return (-1);
+	}
+	else
+	{
+		write(fd, text_content, len);
 	}
 	close(fd);
 
