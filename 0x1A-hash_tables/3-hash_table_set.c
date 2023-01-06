6a03e10fdff336f5f29c5001345119589844b922
Nnamani John <nnamanijoh123@gmail.com>
2023-01-06 23:37:44 +0000

    table get

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -59,7 +59,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *node;
 
-	if (key == NULL)
+	if (ht == NULL || key == NULL)
 		return (0);
 	if (value == NULL)
 		return (0);
