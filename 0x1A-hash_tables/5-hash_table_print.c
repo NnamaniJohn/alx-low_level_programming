8d10f998516115b93dc87aa18a60072a23603148
Nnamani John <nnamanijoh123@gmail.com>
2023-01-07 00:37:56 +0000

    delete

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,14 +10,19 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *node;
+	int tem = 0;
 
-	if (ht)
+	if (ht == NULL)
 		return;
+
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i])
 		{
+			if (tem == 1)
+				printf(", ");
+			tem = 1;
 			node = ht->array[i];
 			while (node)
 			{
