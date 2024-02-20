25e960190fede7ed1ee378599dc6a41dd68edd86
Nnamani John <nnamanijohn123@gmail.com>
2024-02-20 20:04:17 +0100

    atoi modified

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,24 @@
 int _atoi(char *s)
 {
 	int len;
-	int is_negative = 0;
+	int is_negative = 1;
 	int num = 0;
 
 	for(len = 0; *(s + len); len++)
 	{
-		if(*(s + len) >= 48 && *(s + len) <= 58)
+		if(*(s + len) >= '0' && *(s + len) <= '9')
 		{
 			num *= 10;
-			num = num + (*(s + len) - 48);
+			num = num + (*(s + len) - '0');
 		} 
 		else
 		{
 			if(num != 0)
 				break;
 			if(*(s + len) == '-')
-				is_negative = 1;
-			else
-				is_negative = 0;
+				is_negative *= -1;
 		}
 	}
-	if(is_negative)
-		num *= -1;
+	num *= is_negative;
 	return num;
 }
