c89be176abcedcf993440472f7e53b81da833c93
Nnamani John <nnamanijohn123@gmail.com>
2024-02-20 20:16:43 +0100

    atoi modified

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,24 +1,30 @@
+/**
+ * _atoi - converts a string to an integer.
+ * @s: the string to convert
+ * Return: the converted string.
+ */
+
 int _atoi(char *s)
 {
 	int len;
-	int is_negative = 1;
+	int is_negative = -1;
 	int num = 0;
 
-	for(len = 0; *(s + len); len++)
+	for (len = 0; *(s + len); len++)
 	{
-		if(*(s + len) >= '0' && *(s + len) <= '9')
+		if (*(s + len) >= '0' && *(s + len) <= '9')
 		{
 			num *= 10;
-			num = num + (*(s + len) - '0');
-		} 
+			num = num - (*(s + len) - '0');
+		}
 		else
 		{
-			if(num != 0)
+			if (num != 0)
 				break;
-			if(*(s + len) == '-')
+			if (*(s + len) == '-')
 				is_negative *= -1;
 		}
 	}
 	num *= is_negative;
-	return num;
+	return (num);
 }
